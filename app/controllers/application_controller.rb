5bcf6c769d2c86466e6df1af703dc4ebf0fb33fc
kimu805 <kimukimuok805@gmail.com>
2024-10-11 12:44:09 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
