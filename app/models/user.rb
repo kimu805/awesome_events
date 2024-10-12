33f82d95220c9d582702483787723c2503c6c5f8
kimu805 <kimukimuok805@gmail.com>
2024-10-12 17:23:00 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
+
+  def self.from_omniauth
+    
+  end
 end
