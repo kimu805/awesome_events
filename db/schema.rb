de0d4e31ab7e6b86b5f57ee8968bba074b81b74f
kimu805 <kimukimuok805@gmail.com>
2024-10-12 17:09:42 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_11_033937) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_12_080715) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname"
     t.string "email", default: "", null: false
@@ -20,8 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_11_033937) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid", null: false
+    t.string "provider", null: false
+    t.string "image_url", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
