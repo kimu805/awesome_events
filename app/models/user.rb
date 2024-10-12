e1f89defef48933c70a402b1e20e65e22d45ecad
kimu805 <kimukimuok805@gmail.com>
2024-10-12 17:38:00 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,4 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-
-  def self.from_omniauth(auth)
-    binding.pry
-  end
 end
