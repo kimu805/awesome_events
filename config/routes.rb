02e574526b3787ffb6827af27e8c61250747d4c0
kimu805 <kimukimuok805@gmail.com>
2024-10-12 17:15:28 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks",
+    registrations: "user/registrations"
+  }
 end
