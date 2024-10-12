40b1237702129440ee460c40305e5bd7f9ba3028
kimu805 <kimukimuok805@gmail.com>
2024-10-12 17:37:53 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  end
 
-  private
   def authorization
     @user = User.from_omniauth(request.env["omniauth.auth"])
   end
