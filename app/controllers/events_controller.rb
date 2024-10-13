a35f2cb157f85f44f446632db7bdca901b3c428f
kimu805 <kimukimuok805@gmail.com>
2024-10-13 19:30:43 +0900

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "#{@event.name}を作成しました。"
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :place, :content, :start_at, :end_at)
+  end
 end
