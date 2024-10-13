7a82f64b5fab6e636c550e30d4fadd5fabd8f8ec
kimu805 <kimukimuok805@gmail.com>
2024-10-13 19:22:51 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to root_path, alert: "ログインが必要です"
+    end
   end
 end
