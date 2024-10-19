efb8973d337a6d863dda1fe06184f4f4f2f70e1d
kimu805 <kimukimuok805@gmail.com>
2024-10-19 13:41:11 +0900

    Update tickets_controller.rb

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,25 +8,25 @@ class TicketsController < ApplicationController
   
   def create
     @ticket = current_user.tickets.build do |t|
-      t.event = event
+      t.event = @event
       t.comment = params[:ticket][:comment]
     end
     if @ticket.save
-      redirect_to event, notice: "「#{event.name}」に参加を表明しました"
+      redirect_to @event, notice: "「#{@event.name}」に参加を表明しました"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @ticket = current_user.tickets.find_by!(event_id: event.id)
+    @ticket = current_user.tickets.find_by!(event_id: @event.id)
     @ticket.destroy!
-    redirect_to event_path(event), notice: "「#{event.name}」への参加を取り消しました"
+    redirect_to event_path(@event), notice: "「#{@event.name}」への参加を取り消しました"
   end
 
   private
 
   def set_event
-    event = Event.find(params[:event_id])
+    @event = Event.find(params[:event_id])
   end
 end
