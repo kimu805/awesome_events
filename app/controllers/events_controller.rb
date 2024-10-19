d4e5e4376571556041d859b67b7a9993304e80eb
kimu805 <kimukimuok805@gmail.com>
2024-10-19 13:09:23 +0900

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,6 +19,7 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @ticket = @event.tickets.build
+    @tickets = @event.tickets.includes(:user).order(:created_at)
   end
 
   def edit
