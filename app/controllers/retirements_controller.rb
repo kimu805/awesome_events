78179dd075428cfa893013ade2d44492314a7d61
kimu805 <kimukimuok805@gmail.com>
2024-10-19 15:18:25 +0900

    Update retirements_controller.rb

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -6,6 +6,8 @@ class RetirementsController < ApplicationController
   def create
     if current_user.destroy
       redirect_to root_path, notice: "退会完了しました。"
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 end
