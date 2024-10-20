cc612abb7953a760d8c22401b77bd5d4a7aff11d
kimu805 <kimukimuok805@gmail.com>
2024-10-20 11:05:48 +0900

    add method

--- app/models/event.rb
+++ app/models/event.rb
@@ -37,4 +37,8 @@ class Event < ApplicationRecord
       errors.add(:start_at, "は終了時間よりも前に設定してください")
     end
   end
+
+  def remove_image_if_user_accept
+    self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
+  end
 end
